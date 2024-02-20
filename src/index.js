57ff36283bf6b2faff3a3d5fce880da90a66cda5
SaurabhMagara <saurabhmagara@gmail.com>
2024-02-21 01:09:59 +0530

    mini projects added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import {createRoot} from 'react-dom/client';
 import App from "./App";
-import Qoutes from "./Qoutes";
-
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<Qoutes/>);
+root.render(<App/>);
